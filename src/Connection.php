eac4cb835d7540e1c31be21abc01e6c44a4ac234
Piotr Gasidło <p.gasidlo@olczyk.net>
2015-02-11 11:12:07 +0100

    Treat socket_read() === "" as timeout too.
    Fixes previous commit (91beda011c12200656008d9f495414806ac3112e).

--- src/Connection.php
+++ src/Connection.php
@@ -83,7 +83,7 @@ class Connection {
 		$length_left = $length;
 		while ($length_left > 0) {
 			$data_slice = @socket_read($this->connection, $length_left);
-			if ($data_slice === FALSE || $data_slice === 0) {
+			if ($data_slice === FALSE || $data_slice === "") {
 				return $this->connectionException();
 			}
 			$data .= $data_slice;
